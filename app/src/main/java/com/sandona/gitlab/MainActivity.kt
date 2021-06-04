b11f97dbf260745480f5b0d94a4cf79a2ab08040
sandos1 <sandona.soro@yahoo.fr>
2021-06-04 09:27:00 -0400

    master first commit squash

--- app/src/main/java/com/sandona/gitlab/MainActivity.kt
+++ app/src/main/java/com/sandona/gitlab/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        print("Master first changes")
+        print("Master first changes amend")
+
+
     }
 }
